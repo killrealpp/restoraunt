957f7ddcc549bc557a555cb6e0d3b02bbaa68857
killrealpp <kaysarovkirill5@gmail.com>
2025-03-17 01:41:15 +0300

    add adaptiv more

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import { initProgressBarAndSwiper } from "./slider.js";
 import {toggleClassOnClick} from './toggleSvg.js'
 import { toggleMenu } from "./burger.js";
 
-// initDropdown()
+initDropdown()
 initProgressBarAndSwiper()
 toggleClassOnClick('.dishes__cart-heart', 'heart-active');
 toggleClassOnClick('.rest__cart-book', 'book-active');
